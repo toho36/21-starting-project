cd43f7b482e36dac70b44aa283a250708e340ee4
toho36 <toho0036@gmail.com>
2023-06-29 12:20:00 +0200

    304 Creating new Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> }, // property for main page example.com/
-  {},
+  { path: '/products', element: <ProductsPage /> },
 ]);
 function App() {
   return <RouterProvider router={router} />;
