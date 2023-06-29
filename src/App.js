e446840dd2e3ffffd3f82700d8789248f9faef6e
toho36 <toho0036@gmail.com>
2023-06-29 12:43:21 +0200

    keeping the object base approach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
-// const router = createBrowserRouter([
-//   { path: '/', element: <HomePage /> }, // property for main page example.com/
-//   { path: '/products', element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> }, // property for main page example.com/
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
