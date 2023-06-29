8f3962d8d649ae56bcc0e0dabcfa5eabde6a80d7
toho36 <toho0036@gmail.com>
2023-06-29 12:01:18 +0200

    21 multiple page SPA

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+createBrowserRouter([
+  { path: '/' }, // property for main page example.com/
+  {},
+]);
+function App() {
+  return <div></div>;
+}
+
+export default App;
