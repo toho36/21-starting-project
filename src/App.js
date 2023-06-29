38752d4cf2df00ee656bda99334449cc65247764
toho36 <toho0036@gmail.com>
2023-06-29 12:42:41 +0200

    Alternative JSX way to routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }, // property for main page example.com/
-  { path: '/products', element: <ProductsPage /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> }, // property for main page example.com/
+//   { path: '/products', element: <ProductsPage /> },
+// ]);
+
 function App() {
   return <RouterProvider router={router} />;
 }
