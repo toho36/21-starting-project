a21ba861419b3fd8abf82ba753acd423198a7501
toho36 <toho0036@gmail.com>
2023-06-29 12:55:03 +0200

    306 navigating with links
    impact on performance

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function HomePage() {
-  return <h1>My HomePage</h1>;
+  return (
+    <>
+      <h1>My HomePage</h1>
+      <p>
+        Go to <a href="/products"> the list of products</a>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
